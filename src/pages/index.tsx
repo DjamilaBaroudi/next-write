2d2116fdc1699dc06592d47fd34bc12af8b298e7
Djamila Baroudi <db@blksail.ai>
2023-02-16 11:35:10 +0100

    creates the search and tags part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { MdMenu } from 'react-icons/md'
 import { BsBell } from 'react-icons/bs'
 import { FiEdit } from 'react-icons/fi'
+import { CiSearch } from 'react-icons/ci'
 const HomePage = () => {
   return (
     <div className="flex flex-col w-full h-screen">
@@ -25,7 +26,32 @@ const HomePage = () => {
       {/* This is for the body and sidebar */}
       <section className='grid grid-cols-12 place-items-center w-full h-full'>
         <main className='col-span-8 border-r border-gray-300 h-full w-full'>
-          Main Part
+          <div className='flex flex-col space-y-4 w-full p-10'>
+            <div className='flex space-x-4 items-center w-full'>
+              <label htmlFor="search" className='relative w-full border-gray-300 border rounded'>
+                <div className='absolute left-2 h-full flex items-center'>
+                  <CiSearch />
+                </div>
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  className='outline-none  py-1 px-4 pl-8
+                  text-sm placeholder:text-xs placeholder:text-gray-300'
+                  placeholder='Search...' />
+              </label>
+              <div className='flex items-center w-full'>
+                <div>topics: </div>
+                <div className='flex space-x-2 items-center'>
+                  {
+                    Array.from({ length: 4 }).map((_, index) => (
+                      <div key={index} className='rounded-3xl bg-gray-200/50 p-4'>tag {index} </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
         <aside className='col-span-4 h-full w-full'>
           {/* This is for sidebar */}
