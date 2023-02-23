fb6a48156cac1f3e79640afa683553c53f141a1a
Djamila Baroudi <db@blksail.ai>
2023-02-23 15:28:16 +0100

    adds login and logout functionnalities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,11 @@
 import React from 'react'
-import { MdMenu } from 'react-icons/md'
-import { BsBell } from 'react-icons/bs'
-import { FiEdit } from 'react-icons/fi'
 import { CiSearch } from 'react-icons/ci'
 import { HiChevronDown } from 'react-icons/hi'
+import MainLayout from '../layouts/MainLayout'
+
 const HomePage = () => {
   return (
-    <div className="flex flex-col w-full h-full">
-      <header className='h-20 w-full flex flex-row justify-around items-center bg-white border-b-[1px] border-gray-300'>
-        {/* This is for the header */}
-        <div> <MdMenu className='text-2xl text-gray-600' /> </div>
-        <div className='font-thin text-xl'>Ultimate Blog</div>
-        <div className='flex items-center space-x-4'>
-          <div> <BsBell className='text-2xl text-gray-600' /> </div>
-          <div className='bg-gray-500 w-5 h-5 rounded-full'></div>
-          <div>
-            <button className='flex rounded items-center px-4 py-2 space-x-3 border border-gray-200
-             transition hover:border-gray-900 hover:text-gray-900'>
-              <div>Write</div>
-              <div> <FiEdit /> </div>
-            </button>
-          </div>
-        </div>
-      </header>
-
+    <MainLayout>
       {/* This is for the body and sidebar */}
       <section className='grid grid-cols-12 place-items-center w-full h-full'>
         <main className='col-span-8 border-r border-gray-300 h-full w-full px-24'>
@@ -140,13 +122,13 @@ const HomePage = () => {
                       <div className='flex w-full space-x-1 items-center'>
                         <div className='rounded-full bg-gray-300 h-8 w-8 flex-none'></div>
                         <div className='flex text-sm font-semibold'>
-                          Djamila BAROUDI &#x2022; 
+                          Djamila BAROUDI &#x2022;
                         </div>
                         <div > 17 Feb. 2023</div>
                       </div>
                     </div>
                   </div>
- 
+
                 </div>
               ))}
 
@@ -154,7 +136,7 @@ const HomePage = () => {
           </div>
         </aside>
       </section >
-    </div >
+    </MainLayout >
   )
 }
 
