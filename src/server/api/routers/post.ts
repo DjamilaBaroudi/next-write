4f503d11df9870b0e5b5fa58ffa72cf1313838c2
Djamila Baroudi <db@blksail.ai>
2023-02-28 15:48:24 +0100

    mutates the data

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,28 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import slugify from "slugify";
 import { WriteFormSchema } from "../../../components/WriteFormModal";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
     createPost: protectedProcedure.input(
         WriteFormSchema
     ).mutation(
         async ({ ctx: { prisma, session }, input: { title, description, text } }) => {
-            if(!title){
             await prisma.post.create({
                 data: {
                     title,
                     text,
-                    description, 
+                    description,
                     slug: slugify(title),
                     authorId: session.user.id
                 }
             })
-            }
-            else {
-                console.log('title already exists!')
-            }
+        }
+    ),
+    getPosts: publicProcedure.query(
+        async ({ ctx: { prisma } }) => {
+            const posts = await prisma.post.findMany(
+                {
+                    orderBy: {
+                        created_at: 'desc'
+                    },
+                    include: {
+                        author: {
+                            select: {
+                                name: true,
+                                image: true,
+                                
+                            }
+                        }
+                    }
+                }
+            );
+            return posts;
         }
     )
+
 })
